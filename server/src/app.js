76c20b0241ed2b2c756566f4024f914ae440cb78
Anmol Gupta <anmol.gupta91@gmail.com>
2015-06-27 12:16:55 +0530

    [try] separating User control as well

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     db = require('./model/db'),
     bodyParser = require('body-parser'),
-    Users = require('./model/users'),
+    userRouter = require('./routers/userRouter'),
     authenticationRouter = require('./routers/authenticationRouter');
 //    path = require('path'),
 //    favicon = require('serve-favicon'),
@@ -18,52 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8128;
-
-
-var router = express.Router();
-
-router.use(function(req, res, next) {
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-router.get('/', function(req, res) {
-    res.send({ message: 'hooray! welcome to our api!' });   
-});
-
-router.get('/th1/', function(req, res) {
-    res.send("{ message: 'hooray! welcome to our api!' }");   
-});
-
-router.get('/th/', function(req, res) {
-    res.json({ message: 'haye! mera to gaand fat gya' });   
-});
-
-router.post('/',function(req,res) {
-    
-    var msg = req.body.name;  
-    res.json({message: msg+ ' is a great man'});
-    
-});
-
-var userRouter = express.Router();
-userRouter.post('/addNew',function(req, res){
-    
-    var users = new Users();
-    users.name = req.body.name;  
-    users.phoneNumber = req.body.phoneNumber;  
-    users.email = req.body.email;  
-    users.dob = req.body.dob;
-    
-    
-    users.save(function(err){
-        if(err)
-            res.send(err)
-        res.send("save ho gya bc!")
-        
-    });
-    
-});
 
 app.use('/api', router);
 app.use('/authentication',authenticationRouter);
--- server/src/routers/usersRouter.js
+++ server/src/routers/usersRouter.js
@@ -1,4 +1,24 @@
-var Authentication = require('../model/users'),
+var Users = require('../model/users'),
     db = require('../model/db'),
     express = require('express');
 
+var userRouter = express.Router();
+userRouter.post('/addNew',function(req, res){
+    
+    var users = new Users();
+    users.name = req.body.name;  
+    users.phoneNumber = req.body.phoneNumber;  
+    users.email = req.body.email;  
+    users.dob = req.body.dob;
+    
+    
+    users.save(function(err){
+        if(err)
+            res.send(err)
+        res.send("save ho gya bc!")
+        
+    });
+    
+});
+
+module.exports = userRouter;
